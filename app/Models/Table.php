47092e841c9b99993afb8cbccf724770deda9e15
Jacobson <romeojacobson@gmail.com>
2023-08-08 21:21:47 +0300

    Added table controller

--- app/Models/Table.php
+++ app/Models/Table.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Table extends Model
 {
@@ -12,4 +13,9 @@ class Table extends Model
     protected $fillable = [
         'club',
     ];
+
+    public function teams(): HasMany
+    {
+        return $this->hasMany(Team::class);
+    }
 }
