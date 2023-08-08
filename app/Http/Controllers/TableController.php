ba414d90f518b3f6da3c96a665cf993a14fce8b5
Jacobson Loor <72121391+Loorinho@users.noreply.github.com>
2023-08-08 21:34:00 +0300

    Update TableController.php
    Added table fetching method

--- app/Http/Controllers/TableController.php
+++ app/Http/Controllers/TableController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Team;
 
 class TableController extends Controller
 {
-    //
+    public function getTable()
+    {
+        $table = Table::all();
+        return response()->json([
+            'teams'=> $teams                    
+        ]);
+    }
 }
