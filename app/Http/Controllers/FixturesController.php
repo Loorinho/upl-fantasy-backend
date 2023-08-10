5340ef971c61c36de992ef292e7ff10223af141e
Jacobson Loor <72121391+Loorinho@users.noreply.github.com>
2023-08-10 04:46:53 +0300

    Update FixturesController.php
    Added getting all fixtures action in controller

--- app/Http/Controllers/FixturesController.php
+++ app/Http/Controllers/FixturesController.php
@@ -3,8 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Fixtures;
 
 class FixturesController extends Controller
 {
-    //
+    public function getFixtures()
+    {
+        $fixtures = Fixtures::all();
+
+        return response()->json([
+           'fixtures' => $fixtures                     
+        ]);
+    }
 }
