e0d8fe8766e3db9a24be9a2b90c58a4c0008325d
Jacobson Loor <72121391+Loorinho@users.noreply.github.com>
2023-08-10 05:10:32 +0300

    Update 2023_08_10_014113_create_fixtures_table.php
    Added game week field

--- database/migrations/2023_08_10_014113_create_fixtures_table.php
+++ database/migrations/2023_08_10_014113_create_fixtures_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('fixtures', function (Blueprint $table) {
             $table->id();
+            $table->integer('game_week');
             $table->integer('home_team');
             $table->integer('away_team');
             $table->string('stadium');
