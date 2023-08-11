3611aef1dd87eaded1b000b8e402b3bfc69e34dd
Jacobson Loor <72121391+Loorinho@users.noreply.github.com>
2023-08-11 04:01:00 +0300

    Update FixturesController.php
    Added create fixture action in controller

--- app/Http/Controllers/FixturesController.php
+++ app/Http/Controllers/FixturesController.php
@@ -7,6 +7,22 @@ use App\Models\Fixtures;
 
 class FixturesController extends Controller
 {
+     public function createFixtures(Request $request)
+    {
+        $fixture = Fixtures::create([
+            'season' =>  $request->season,
+             'game_week' =>  $request->gameWeek,
+             'home_team' => $request->homeTeam,
+             'away_team' => $request->awayTeam,
+             'stadium' => $request->stadium,
+             'date' => $request->date,
+             'time' => $request->time,                          
+        ]);
+
+        return response()->json([
+           'message' => $fixture . " of ". $fixture->home_team . " vs ". $fixture->away_team . " created successfully"                      
+        ]);
+    }
     public function getFixtures()
     {
         $fixtures = Fixtures::all();
