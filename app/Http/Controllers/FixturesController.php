86fe01e7021c312d5fd8c6769be9b7fce15debcb
Jacobson Loor <72121391+Loorinho@users.noreply.github.com>
2023-08-20 18:23:04 +0300

    Update FixturesController.php
    Added fixture update action

--- app/Http/Controllers/FixturesController.php
+++ app/Http/Controllers/FixturesController.php
@@ -33,4 +33,29 @@ class FixturesController extends Controller
             'fixtures' => $fixtures
         ]);
     }
+
+        public function updateFixture(Request $request)
+    {
+        $fixture = Fixtures::where('id', '=', $id)
+                update([
+            'season' =>  $request->season,
+            'game_week' =>  $request->gameWeek,
+            'home_team' => $request->homeTeam,
+            'away_team' => $request->awayTeam,
+            'stadium' => $request->stadium,
+            'date' => $request->date,
+            'time' => $request->time,
+        ]);
+
+        if(!$fixture){
+              return response()->json([
+                'message' => "The fixture could not be updated. Try again later"
+            ], 400);    
+        }
+        $fixtures = Fixtures::all();
+        return response()->json([
+            'message' => "The fixture of " . $fixture->home_team . " vs " . $fixture->away_team . " updated successfully",
+            'fixtures' => $fixtures
+        ]);
+    }
 }
